7fbdb1dbaa61178a8628371671571b3d2bb9ca0f
toritori0318 <toritori0318@gmail.com>
2015-02-23 15:08:30 +0900

    ryotaraiさんのコメント反映分

--- recipes/td-agent/attribute.rb
+++ recipes/td-agent/attribute.rb
@@ -1,6 +1,18 @@
-node["td_agent"] = node["td_agent"] || {}
+# デフォルトattribute
+node.reverse_merge!(
+  td_agent: {
+    includes: [],
+    plugins: %w{
+      forest
+    },
+  },
+)
 
-node['td_agent']['includes'] = node['td_agent']['includes'] || []
-node["td_agent"]["plugins"]  = node['td_agent']['plugins'] || %w{
-  forest
-}
+# node.reverse_merge! は以下と同じようなことを実現している
+#
+#   node["td_agent"] = node["td_agent"] || {}
+#   node['td_agent']['includes'] = node['td_agent']['includes'] || []
+#   node["td_agent"]["plugins"]  = node['td_agent']['plugins'] || %w{
+#     forest
+#   }
+#
