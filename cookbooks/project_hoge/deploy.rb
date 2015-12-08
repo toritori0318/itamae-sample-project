c85ac5f116debb25e33e8e5ad39ab5d2a7113f2a
toritori0318 <toritori0318@gmail.com>
2015-12-08 19:43:06 +0900

    deploy.rb ちょいと修正

--- cookbooks/project_hoge/deploy.rb
+++ cookbooks/project_hoge/deploy.rb
@@ -1,12 +1,28 @@
-app_root = "/app/hoge"
+app_root    = "/app/hoge"
+app_root_c  = "#{app_root}/current"
+target_repo = "https://github.com/toritori0318/drone-fig-ci-sample.git"
 
 # create directory
 directory app_root
 
-# deploy
-git "#{app_root}/current" do
-  repository "https://github.com/toritori0318/drone-fig-ci-sample.git"
-  revision "master"
+target_branch = "master"
+# git pull
+execute "git checkout #{target_branch}" do
+  command <<-"EOH"
+set -e
+export dir=#{app_root_c}
+if [ -d $dir ]; then
+    cd $dir
+    git fetch origin
+    git checkout #{target_branch}
+    git reset --hard origin/#{target_branch}
+else
+    cd #{app_root}
+    git clone #{target_repo} $dir
+    cd $dir
+    git checkout #{target_branch}
+fi
+EOH
 end
 
 # ...
